26d7304ea63be2719c76af4d6ae56a17a0daeb49
unknown <cb.sc.p2cse24008@cb.students.amrita.edu>
2026-06-12 11:37:03 +0530

    merge

--- List_Comprehension.py
+++ List_Comprehension.py
@@ -11,14 +11,14 @@ squared_list = [i ** 2 for i in list_1]
 print(squared_list)
 
 with open("python_entries.txt", "a") as python_file:
-    python_file.write(str(squared_list))
+    python_file.write(str(squared_list)+"\n")
 
-tv_shows = ["breaking bad", "game of Thrones", "stranger Things", "The Crown", "The Mandalorian"]
 
-captalised_tv_shows = [i.title() for i in tv_shows]
+tv_shows = ["breaking bad", "game of Thrones", "stranger Things", "The Crown", "The Mandalorian", "The office"]
+
+
+captalised_tv_shows = [i.title() for i in tv_shows if len(i) >=10]
 print(captalised_tv_shows)
-
-print("\n")
-
+    
 with open("python_entries.txt", "a") as python_file:
     python_file.write(str(captalised_tv_shows))
