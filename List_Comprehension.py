5cb6a2bf87f5d8c7d0d6285f9d73dc537e593fdb
unknown <cb.sc.p2cse24008@cb.students.amrita.edu>
2026-06-12 11:09:41 +0530

    list_comprehension

---
+++ List_Comprehension.py
@@ -0,0 +1,18 @@
+# list comprehension is a concise way to create lists. It consists of brackets containing an expression followed by a for clause, then zero or more for or if clauses. The expressions can be anything, meaning you can put in all kinds of objects in lists.
+list_1 = [1,2,3,4,5]
+
+squared_list = []
+for i in list_1:
+    square = i ** 2
+    squared_list.append(square)
+print(squared_list)
+
+squared_list = [i ** 2 for i in list_1]
+print(squared_list)
+
+with open("python_entries.txt", "a") as python_file:
+    python_file.write(str(squared_list))
+
+    
+
+
